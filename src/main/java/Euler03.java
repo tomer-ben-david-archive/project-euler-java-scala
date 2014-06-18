109fe258f17e7125a9d142e2d39d3c7d73da3694
tomerbd1 <tomerbd1@gmail.com>
2014-06-18 17:18:32 +0300

    added description for project 3 in euler

--- src/main/java/Euler03.java
+++ src/main/java/Euler03.java
@@ -3,6 +3,11 @@
  * on 18/06/14.
  */
 public class Euler03 {
+    /**
+     * Start dividing the n number by 2 if ok continue dividing by 2 when cant start divding by 3
+     * The largest number you encounter would be a prime number because if its not you would already
+     * be able to divide it by 2 or 3 or one of its smaller dividers.
+     */
     private static int largestPrimeFactor(int compositeN) {
         int i;
         for (i = 2; i < compositeN; i++) {
